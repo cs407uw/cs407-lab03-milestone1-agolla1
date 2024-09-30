7a73211a0be3ce899737fc13e606b605b279fa40
akshangolla <shivsaicool@gmail.com>
2024-09-30 13:33:51 -0500

    Milestone 1 submission

--- app/src/main/java/com/cs407/lab3/milestone1/MainActivity.kt
+++ app/src/main/java/com/cs407/lab3/milestone1/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.cs407.lab3.milestone1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.view.MenuItem
+import android.view.Menu
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val submitButton = findViewById<Button>(R.id.submitbutton)
+        submitButton.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Dialog Title")
+            .setMessage("This is a dialog message.")
+            .setPositiveButton("OK") {dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.example_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item1 -> {
+                Toast.makeText(this, "item 1 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.item2 -> {
+                Toast.makeText(this, "item 2 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.item3 -> {
+                Toast.makeText(this, "item 3 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.subitem1 -> {
+                Toast.makeText(this, "SubItem 1 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.subitem2 -> {
+                Toast.makeText(this, "SubItem 2 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
+
